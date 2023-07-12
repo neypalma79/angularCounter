3101beab36e36a6365cb5cbc050539f1721ae43a
Ney Palma <ney_palma@yahoo.com>
2023-07-12 16:29:16 -0500

    agregado submodulo productos - componente consulta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { ListaComponent } from './lista/lista.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { ConsultaComponent } from './consulta/consulta.component';
+import { ProductosModule } from './modules/productos/productos.module';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, ListaComponent, HeroesComponent, ConsultaComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    ListaComponent,
+    HeroesComponent,
+  ],
+  imports: [BrowserModule, ProductosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/modules/productos/productos.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConsultaComponent } from '../../consulta/consulta.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [ConsultaComponent],
+  declarations: [ConsultaComponent],
+  providers: [],
+})
+export class ProductosModule {}
