ed74d72d26683d63abf2dda63b0e6a76c9b4d08e
Ney Palma <ney_palma@yahoo.com>
2023-07-12 16:04:25 -0500

    terminado consulta productos

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class ConsultaComponent {
   private valorBusqueda: string = '';
+  private productos: Producto[] = [
+    {
+      nombre: 'martillo',
+      marca: 'trouper',
+      descripcion: 'martillo mango de goma',
+      precio: 12,
+    },
+    {
+      nombre: 'martillo',
+      marca: 'bosch',
+      descripcion: 'martillo mango de madera',
+      precio: 20,
+    },
+    {
+      nombre: 'taladro',
+      marca: 'trouper',
+      descripcion: 'con percutor',
+      precio: 40,
+    },
+  ];
+  public productosBusqueda: Producto[] = [];
 
   get getValorBusqueda(): string {
     return this.valorBusqueda;
@@ -14,7 +35,19 @@ export class ConsultaComponent {
 
   public guardarValorBusqueda(event: any) {
     const { value } = event.target;
-    console.info(value);
     this.valorBusqueda = value;
   }
+
+  public getProductos(): void {
+    this.productosBusqueda = this.productos.filter((producto) => {
+      return producto.nombre.toUpperCase() === this.valorBusqueda.toUpperCase();
+    });
+  }
+}
+
+interface Producto {
+  nombre: string;
+  marca: string;
+  descripcion: string;
+  precio: number;
 }
